b4cb4b3f13434c62c125da61f5132f47248aad0e
Chandrakant Naik <chandrakantnaik789@gmail.com>
2023-10-13 23:10:19 +0530

    improved validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,56 +2,58 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  loginForm!: FormGroup
-  loginFormSubmit= false;
+  loginForm!: FormGroup;
+  loginFormSubmit = false;
   constructor(
-    private formbuilder : FormBuilder,
-    private http : HttpClient,
+    private formbuilder: FormBuilder,
+    private http: HttpClient,
     private router: Router
-    ){ }
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.formbuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
-  login(){
-
-    if(this.loginForm.invalid){
+  login() {
+    if (this.loginForm.invalid) {
       return;
     }
 
-   this.loginFormSubmit = true;
+    this.loginFormSubmit = true;
 
-   const loginObj = {
-    email: this.loginForm.value.username,
-    password: this.loginForm.value.password
-   }
+    const loginObj = {
+      email: this.loginForm.value.username,
+      password: this.loginForm.value.password,
+    };
 
-   this.http.post<any>('http://localhost:3004/login', loginObj).subscribe(({accessToken, user}) => {
-
-    localStorage.setItem('token', accessToken);
-    localStorage.setItem('user', JSON.stringify(user));
-
-    if(user){
-      alert('Login Successful');
-      this.loginForm.reset();
-
-      this.router.navigateByUrl('/account/profile');
-    }
-    else{
-      alert('User not found');
-    }
-   })
-   
+    this.http
+      .post<any>('http://localhost:3004/login', loginObj)
+      .pipe(
+        catchError((error) => {
+          alert('User not found');
+          
+          return of(null);
+        })
+      )
+      .subscribe(({ accessToken, user }) => {
+        localStorage.setItem('token', accessToken);
+        localStorage.setItem('user', JSON.stringify(user));
+        if (user) {
+          alert('Login Successful');
+          this.loginForm.reset();
+          this.router.navigateByUrl('/account/profile');
+        }
+      });
   }
 }
