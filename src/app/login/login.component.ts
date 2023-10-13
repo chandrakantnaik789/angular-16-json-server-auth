ade2e45d549718d5e90d2d09f8a201d2347a2998
Chandrakant Naik <chandrakantnaik789@gmail.com>
2023-10-13 22:56:58 +0530

    profie update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,8 @@ export class LoginComponent {
   loginFormSubmit= false;
   constructor(
     private formbuilder : FormBuilder,
-    private http : HttpClient
+    private http : HttpClient,
+    private router: Router
     ){ }
 
   ngOnInit() {
@@ -23,22 +25,33 @@ export class LoginComponent {
   }
 
   login(){
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
    this.loginFormSubmit = true;
-   this.http.get<any>('http://localhost:3000/signUpUsers').
-   subscribe(res => {
-    const user = res.find((a : any) =>{
-      return a.username === this.loginForm.value.username && a.password === this.loginForm.value.password
-    });
+
+   const loginObj = {
+    email: this.loginForm.value.username,
+    password: this.loginForm.value.password
+   }
+
+   this.http.post<any>('http://localhost:3004/login', loginObj).subscribe(({accessToken, user}) => {
+
+    localStorage.setItem('token', accessToken);
+    localStorage.setItem('user', JSON.stringify(user));
+
     if(user){
       alert('Login Successful');
       this.loginForm.reset();
+
+      this.router.navigateByUrl('/account/profile');
     }
     else{
       alert('User not found');
     }
-   },
-   err =>{
-    alert("something went wrong");
    })
+   
   }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,15 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
-  profilePicture: string = '';
-  bio:string= '';
-  uploadProfile(){
 
-  }
-  updateProfile(){
+  profileForm!: FormGroup;
 
-  }
-  skipProfile(){
+  constructor(private fb: FormBuilder, private http : HttpClient,) {}
 
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      profilePicture: [''],
+      fullname: ['', Validators.required],
+      phonenumber: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      bio: [''],
+      password: [''],
+      confirmpassword: [''],
+      id: ['']
+    });
+
+    const user = JSON.parse(localStorage.getItem('user')!);
+
+    this.profileForm.patchValue(user);
   }
+
+  onSubmit() {
+    if (!this.profileForm.valid) {
+      return
+    }
+
+    const {id, confirmpassword} = this.profileForm.value;
+
+    this.profileForm.patchValue({ password: confirmpassword });
+
+    this.http.put('http://localhost:3004/users/'+id, this.profileForm.value).subscribe(v=> {
+      this.updateValue(v);
+      alert('user successfully updated');
+    });
+  }
+
+  updateValue(user: any){
+    localStorage.setItem('user', user);
+    this.profileForm.patchValue(user);
+  }
+
+  onSkip() {
+    // Handle the "Skip" button click event
+  }
+
 }
